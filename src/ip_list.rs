685f6daa28f4103a97baa6b34fc1bb4dd3bcdb09
Paul Colomiets <paul@colomiets.name>
2017-10-13 02:42:47 +0300

    Add `IpList::with_port` helper method

--- src/ip_list.rs
+++ src/ip_list.rs
@@ -1,11 +1,12 @@
 //! IpList type which is a list of ip addresses and helper structures to
 //! work with ip lists
 //!
-use std::net::IpAddr;
+use std::net::{IpAddr, SocketAddr};
 use std::slice::Iter;
 use std::sync::Arc;
 
 use rand::{thread_rng, Rng};
+use addr::Address;
 
 /// IpList is a wrapper type around `Vec<IpAddr>` which serves the same
 /// role as the `Address` but for resolving hostnames (or `A` records) instead
@@ -29,6 +30,16 @@ impl IpList {
             return None
         }
         thread_rng().choose(&self.0[..]).map(|&x| x)
+    }
+
+    /// Iterate over IP addresses in the list
+    pub fn iter(&self) -> IpIterator {
+        IpIterator(self.0.iter())
+    }
+
+    /// Create an `Address` object by attaching the specified to all addresses
+    pub fn with_port(&self, port: u16) -> Address {
+        self.iter().map(|x| SocketAddr::new(*x, port)).collect()
     }
 }
 
